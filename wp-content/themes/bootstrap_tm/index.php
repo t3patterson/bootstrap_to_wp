9b77125c47fc74ae13cdf78e3d67dce90c464ceb
travis <t3patterson@gmail.com>
2015-12-07 10:00:50 -0600

    configuring bootstrap.min.css for glyphicons

--- wp-content/themes/bootstrap_tm/index.php
+++ wp-content/themes/bootstrap_tm/index.php
@@ -30,12 +30,12 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <?php wp_head(); ?>
+    <?php wp_head();     ?>
   </head>
 
   <body>
     <?php get_header(); ?>
-
+    
 
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
@@ -50,7 +50,7 @@
       <!-- Example row of columns -->
       <div class="row">
         <div class="col-md-4">
-          <h2>Heading</h2>
+          <h2>Heading </h2>
           <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
           <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
         </div>
@@ -64,9 +64,6 @@
           <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
           <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
         </div>
-      </div>
-
-      
     </div> <!-- /container -->
     
     <?php get_footer(); ?>

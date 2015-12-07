6d80389100cacd7bb26a10bd044a9f4c99a4f6ff
travis <t3patterson@gmail.com>
2015-12-06 21:46:40 -0600

    configuring functions.php for theme_style

---
+++ wp-content/themes/bootstrap_tm/footer.php
@@ -0,0 +1,5 @@
+<hr>
+
+<footer>
+  <p>&copy; 2015 Company, Inc.</p>
+</footer>
--- wp-content/themes/bootstrap_tm/index.php
+++ wp-content/themes/bootstrap_tm/index.php
@@ -30,34 +30,12 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <?php wp_head(); ?>
   </head>
 
   <body>
+    <?php get_header(); ?>
 
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <form class="navbar-form navbar-right">
-            <div class="form-group">
-              <input type="text" placeholder="Email" class="form-control">
-            </div>
-            <div class="form-group">
-              <input type="password" placeholder="Password" class="form-control">
-            </div>
-            <button type="submit" class="btn btn-success">Sign in</button>
-          </form>
-        </div><!--/.navbar-collapse -->
-      </div>
-    </nav>
 
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
@@ -88,13 +66,10 @@
         </div>
       </div>
 
-      <hr>
-
-      <footer>
-        <p>&copy; 2015 Company, Inc.</p>
-      </footer>
+      
     </div> <!-- /container -->
-
+    
+    <?php get_footer(); ?>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
